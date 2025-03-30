44ae9e9b1ceb68c782a452949a913cc8a4beeea1
adityakumartiwary <129543328+adityakumartiwary@users.noreply.github.com>
2025-03-30 11:09:10 +0530

    Time: 1 ms (19.39%), Space: 26.8 MB (14.86%) - LeetHub

---
+++ 3832-minimum-cost-to-reach-every-position/3832-minimum-cost-to-reach-every-position.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> minCosts(vector<int>& cost) {
+        int n=cost.size();
+        vector<int>ans(n,0);
+        ans[0]=cost[0];
+        int minCost = cost[0];
+
+
+        for(int i=0;i<n;i++){
+            minCost=min(minCost,cost[i]);
+            ans[i]=minCost;
+        }
+
+        return ans;
+    }
+};
