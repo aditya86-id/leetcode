7db1b13830bf4aa0a43362d410129b931f33ecf7
Aditya Tiwary <adityatiwary888@gmail.com>
2025-08-06 06:44:03 +0530

    Time: 7 ms (73.5%), Space: 44.7 MB (74.73%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int n=g.size();
+        int m=s.size();
+        int maxT=0;
+
+        int i=0,j=0;
+
+        while(i<n && j<m){
+            if(s[j]>=g[i])
+            {
+                maxT++;
+                i++;
+            }
+
+            j++;
+        }
+
+        return maxT;
+    }
+};
