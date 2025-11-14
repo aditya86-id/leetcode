5ccd693fc7527eef0873c8a2022089e2909af1b2
Aditya Tiwary <adityatiwary888@gmail.com>
2025-11-14 23:29:03 +0530

    Time: 4 ms (60.77%), Space: 28.6 MB (35.02%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    vector<int> nextGreaterElements(vector<int>& nums) {
-        int n=nums.size();
+    vector<int> nextGreaterElements(vector<int>& a) {
+        int n = a.size();
         vector<int>v(n,-1);
 
         stack<int>st;
-        for(int i=2*n-1;i>=0;i--)
+        for(int i = 2*n - 1; i >= 0; i--)
         {
-            while(!st.empty()&&(nums[i%n]>=st.top()))
+            // we pop out all elements smaller than current element
+            while(!st.empty() && (a[i%n] >= st.top()))
             {
                 st.pop();
             }
 
-            if(!st.empty()&&i<n)
-                v[i]=st.top();
-            
-            st.push(nums[i%n]);
+            // if stack is empty means no greater element is there
+            // if not empty we make answer at that index equal to top element
+            if(!st.empty() && (i < n))
+            {
+                v[i] = st.top();
+            }
+
+            st.push(a[i%n]);
         }
 
-        return  v;
+        return v;
     }
 };
