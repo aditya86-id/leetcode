5d1c3887d2233b545a4d52820736a0bfcd2153be
Aditya Tiwary <adityatiwary888@gmail.com>
2025-11-14 23:56:49 +0530

    Time: 0 ms (100%), Space: 28.7 MB (98.51%) - LeetHub

---
+++ 1388-greatest-sum-divisible-by-three/1388-greatest-sum-divisible-by-three.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int maxSumDivThree(vector<int>& nums) {
+        int total = 0;
+        int min1_1 = 1e4, min1_2 = 1e4; // smallest and 2nd smallest numbers with remainder 1
+        int min2_1 = 1e4, min2_2 = 1e4; // smallest and 2nd smallest numbers with remainder 2
+
+        for (int num : nums) {
+            total += num;
+            int rem = num % 3;
+            if (rem == 1) {
+                if (num < min1_1) {
+                    min1_2 = min1_1;
+                    min1_1 = num;
+                } else if (num < min1_2) {
+                    min1_2 = num;
+                }
+            } else if (rem == 2) {
+                if (num < min2_1) {
+                    min2_2 = min2_1;
+                    min2_1 = num;
+                } else if (num < min2_2) {
+                    min2_2 = num;
+                }
+            }
+        }
+
+        if (total % 3 == 0) return total;
+
+        if (total % 3 == 1) {
+            int op1 = min1_1;
+            int op2 = min2_1 + min2_2;
+            return total - min(op1, op2);
+        }
+
+        // total % 3 == 2
+        int op1 = min2_1;
+        int op2 = min1_1 + min1_2;
+        return total - min(op1, op2);
+    }
+};
