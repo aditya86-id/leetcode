fdb56fc46d490fc91d61c1f62e3e1419b324d091
Aditya Kumar Tiwary <adityatiwary888@gmail.com>
2026-08-05 21:45:42 +0530

    Time: 0 ms (100%), Space: 10.4 MB (97.77%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
-vector<vector<int>> dp;
+    bool canSplit(vector<int>& nums, int k, int maxSum) {
+        int subarrays = 1;
+        long long currSum = 0;
 
-    int maxSum(vector<int>& pref, int idx, int n, int k) {
-        if (k == 1) return pref[n - 1] - (idx > 0 ? pref[idx - 1] : 0);
+        for (int num : nums) {
+            if (currSum + num > maxSum) {
+                subarrays++;
+                currSum = num;
 
-        if (dp[idx][k] != -1) return dp[idx][k];
-
-        int sum = INT_MAX, leftSum;
-        int low = idx, high = n - k;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            leftSum = pref[mid] - (idx > 0 ? pref[idx - 1] : 0);
-            int rightSum = maxSum(pref, mid + 1, n, k - 1);
-            sum = min(sum, max(leftSum, rightSum));
-
-            if (leftSum < rightSum) low = mid + 1;
-            else high = mid - 1;
+                if (subarrays > k)
+                    return false;
+            } else {
+                currSum += num;
+            }
         }
 
-        return dp[idx][k] = sum;
+        return true;
     }
-    int splitArray(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int>pref(n);
-        pref[0]=nums[0];
-        for(int i=1;i<n;i++) pref[i]=pref[i-1]+nums[i];
-        dp.assign(n,vector<int>(k+1,-1));
 
-        return maxSum(pref,0,n,k);
+    int splitArray(vector<int>& nums, int k) {
+        int low = *max_element(nums.begin(), nums.end());
+        long long high = 0;
+
+        for (int num : nums)
+            high += num;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (canSplit(nums, k, mid))
+                high = mid;
+            else
+                low = mid + 1;
+        }
+
+        return low;
     }
 };
