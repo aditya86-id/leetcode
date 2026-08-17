5c793e091407c51b8238b02fbf5cdffad66a5713
Aditya Kumar Tiwary <adityatiwary888@gmail.com>
2026-08-17 16:30:24 +0530

    Time: 0 ms (100%), Space: 9.3 MB (54.05%) - LeetHub

---
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string fractionToDecimal(int numerator, int denominator) {
+        if(numerator==0)
+            return "0";
+
+        string fraction;
+        if((numerator<0)^(denominator<0))
+            fraction+="-";
+
+        long long divident = llabs((long long)numerator);
+        long long divisor = llabs((long long)denominator);
+        fraction+=to_string(divident/divisor);
+        long long remainder = divident%divisor;
+        if(remainder==0){
+            return fraction;
+        }
+
+        fraction+=".";
+        unordered_map<long long,int>map;
+        while(remainder!=0){
+
+            if(map.count(remainder)){
+                fraction.insert(map[remainder],"(");
+                fraction+=")";
+                break;
+            }
+
+            map[remainder]=fraction.size();
+            remainder*=10;
+            fraction+=to_string(remainder/divisor);
+            remainder%=divisor;
+        }
+
+        return fraction;
+    }
+};
